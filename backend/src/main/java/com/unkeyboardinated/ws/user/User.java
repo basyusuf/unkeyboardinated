5aeef7ff048a04836d9acd670ec4c4a84461a461
Yusuf Baş <y.penava@hotmail.com>
2021-06-27 23:31:15 +0300

    Added create user  validation with Bean

--- backend/src/main/java/com/unkeyboardinated/ws/user/User.java
+++ backend/src/main/java/com/unkeyboardinated/ws/user/User.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -13,8 +16,19 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
+	@NotNull
+	@Size(min=4,max=50)
+	@UniqueUsername
 	private String username;
+	
+	@NotNull
+	@Size(min=4,max=120)
 	private String displayName;
+	
+	@NotNull
+	@Size(min=8,max=255)
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
 	private String password;
 	
 }
